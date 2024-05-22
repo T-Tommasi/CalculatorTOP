eddf30ee4d25f046324bb141bbe44f7da93d6d06
T-Tommasi <ttommasi1945@gmail.com>
2024-05-22 19:38:38 +0200

    (Fix) - fixed missing variables for visualization
    Fixed missing variables for some elemenets
    Fixed the HTML and created different table for symbols
    TODO: create an actually working CSS

---
+++ calculatorLogic.js
@@ -0,0 +1,27 @@
+//Create a function for each of the basic mathematical operations
+
+function add(firstNumber,secondNumber) {
+    return firstNumber + secondNumber
+};
+
+function subtract(firstNumber,secondNumber){
+    return firstNumber - secondNumber
+};
+
+function multiplication(firstNumber,secondNumber){
+    return firstNumber * secondNumber
+};
+
+function division(firstNumber,secondNumber){
+    return firstNumber*secondNumber
+}
+
+//Variables for showing the results on the calculator
+const plus = '+';
+const minus = '-';
+const multiply = '*';
+const divisionSign = '/';
+let numberA = '';
+let numberB = '';
+
+//
