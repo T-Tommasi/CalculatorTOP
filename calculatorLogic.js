9d955b27a87cd531cf7c0d8b08593c89b8c61dfe
T-Tommasi <ttommasi1945@gmail.com>
2024-05-23 20:18:36 +0200

    (FEAT) Created listeners for the calc. buttons
    - Added a variable declared for each button
    - Added event lisneters for clicking the various buttons
    - Added a function to push the new numbers into the calculation array

--- calculatorLogic.js
+++ calculatorLogic.js
@@ -7,17 +7,48 @@ let numberA = '';
 let numberB = '';
 
 //Get the various numerical buttons
-let 0 = document.getElementById('#number0');
-let 1 = document.getElementById('#number1');
-let 2 = document.getElementById('#number2');
-let 3 = document.getElementById('#number3');
-let 4 = document.getElementById('#number4');
-let 5 = document.getElementById('#number5');
-let 6 = document.getElementById('#number6');
-let 7 = document.getElementById('#number7');
-let 8 = document.getElementById('#number8');
-let 9 = document.getElementById('#number9');
+let buttonZero = document.getElementById('#number0');
+let buttonOne = document.getElementById('#number1');
+let buttonTwo = document.getElementById('#number2');
+let buttonThree = document.getElementById('#number3');
+let buttonFour = document.getElementById('#number4');
+let buttonFive = document.getElementById('#number5');
+let buttonSix = document.getElementById('#number6');
+let buttonSeven = document.getElementById('#number7');
+let buttonEight = document.getElementById('#number8');
+let buttonNine = document.getElementById('#number9');
 
+//Add eventListener for click on all the elements before
+buttonZero.addEventListener('click', function () {
+    addNumberToScreen(0);
+});
+buttonOne.addEventListener('click', function () {
+    addNumberToScreen(1);
+});
+buttonTwo.addEventListener('click', function () {
+    addNumberToScreen(2);
+});
+buttonThree.addEventListener('click', function () {
+    addNumberToScreen(3);
+});
+buttonFour.addEventListener('click', function () {
+    addNumberToScreen(4);
+});
+buttonFive.addEventListener('click', function () {
+    addNumberToScreen(5);
+});
+buttonSix.addEventListener('click', function () {
+    addNumberToScreen(6);
+});
+buttonSeven.addEventListener('click', function () {
+    addNumberToScreen(7);
+});
+buttonEight.addEventListener('click', function () {
+    addNumberToScreen(8);
+});
+buttonNine.addEventListener('click', function () {
+    addNumberToScreen(9);
+});
 
 //Create a function for each of the basic mathematical operations
 
