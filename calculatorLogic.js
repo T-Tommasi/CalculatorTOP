afea6db88a377162d04997df90b3323276634abb
T-Tommasi <ttommasi1945@gmail.com>
2024-05-23 19:38:10 +0200

    (FEAT) Finished styling the graphical side of the calculator
    - Added some better shading for the calculator itself
    - Fixed and refactored the CSS code
    -

--- calculatorLogic.js
+++ calculatorLogic.js
@@ -1,3 +1,25 @@
+//Variables for showing the results on the calculator
+const plus = '+';
+const minus = '-';
+const multiply = '*';
+const divisionSign = '/';
+let numberA = '';
+let numberB = '';
+
+//Add listeners for all the various keys and for keystrokes
+let 0 = document.getElementById('#number0');
+let 1 = document.getElementById('#number1');
+let 2 = document.getElementById('#number2');
+let 3 = document.getElementById('#number3');
+let 4 = document.getElementById('#number4');
+let 5 = document.getElementById('#number5');
+let 6 = document.getElementById('#number6');
+let 7 = document.getElementById('#number7');
+let 8 = document.getElementById('#number8');
+let 9 = document.getElementById('#number9');
+
+let minusButton = document.getElementById('#minus')
+
 //Create a function for each of the basic mathematical operations
 
 function add(firstNumber,secondNumber) {
@@ -15,13 +37,5 @@ function multiplication(firstNumber,secondNumber){
 function division(firstNumber,secondNumber){
     return firstNumber*secondNumber
 }
-
-//Variables for showing the results on the calculator
-const plus = '+';
-const minus = '-';
-const multiply = '*';
-const divisionSign = '/';
-let numberA = '';
-let numberB = '';
 
 //
