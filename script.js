5b8a162285db0f607a1cea7de28c40aa8eb5ceb4
T-Tommasi <ttommasi1945@gmail.com>
2024-05-31 19:48:49 +0200

    (FEAT) Refractored value calculation coe
    - Removed old buggy calculation code
    - Refractored code to convert array values into numbers
    - Isolated the result variable inside of it's specific array

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ const NUMBERS = [
     {origin: document.querySelector('#number7'), value: 7},
     {origin: document.querySelector('#number8'), value: 8},
     {origin: document.querySelector('#number9'), value: 9},
-]
+];
 
 const OPERATORS = [
     {origin: document.querySelector('#plus'), value: '+'},
@@ -26,76 +26,29 @@ const OPERATORS = [
     {origin: document.querySelector('#multiplication'), value: '*'},
     {origin: document.querySelector('#divided'), value: '/'},
     {origin: document.querySelector('#equals'), value: '='},
-]
+];
 
 const SPECIALS = [
     {origin: document.querySelector('#dot'), value: '.'},
     {origin: document.querySelector('#double0'), value: '00'}
-]
+];
 
-function numberListener (object, number) {
-    object.addEventListener('click', function () {
-        if (memory.operator.length === 0) {
-            memory.firstNumber.push(number); //push the used number to memory
-        } else if (memory.operator.length != 0) {
-            memory.secondNumber.push(number) //if there is an operator selected push to the second number
-        } else {
-            return alert('Critical memory error')
-        }
-        console.log(object);
-        return
-    })
-}
-
-function sum (firstNumber, secondNumber) {
-    return firstNumber + secondNumber; //Function for sum
-};
-function minus (firstNumber, secondNumber) {
-    return firstNumber - secondNumber; //Function for minus
-};
-function multiply (firstNumber, secondNumber) {
-    return firstNumber * secondNumber; //Function for multiply
-};
-function divide (firstNumber, secondNumber) {
-    return firstNumber / secondNumber; //Function for division
-};
-
-function operatorChecker (usedOperator) {
-    let result = '';
-        switch (usedOperator) {
-            case '+':
-                result = sum(memory.firstNumber.map(Number), memory.secondNumber.map(Number))
-                break;
-            case '-':
-                result = minus(memory.firstNumber.map(Number), memory.secondNumber.map(Number))
-                break;
-            case '*':
-                result = multiply(memory.firstNumber.map(Number), memory.secondNumber.map(Number))
-                break;
-            case '/':
-                result = divide(memory.firstNumber.map(Number), memory.secondNumber.map(Number))
-                break;
-            default:
-                alert('Digit a number to start calculating!')
-            }
+function sum (a, b) {
+    let result = Number(a) + Number(b);
     return result
-}
+};
 
-function operatorListener (object) {
-    object.origin.addEventListener('click', function () {
-        if (memory.operator === '') {
-            memory.operator = object.value
-            return
-        } else if (memory.operator != '') {
-            console.log(operatorChecker(memory.operator));
-            memory.operator = '';
-            memory.firstNumber = [];
-            memory.secondNumber = [];
-            return
-        }
-    })
-}
+function minus (a, b) {
+    let result = Number(a) - Number(b);
+    return result
+};
 
-//set up listeners
-NUMBERS.forEach(item => numberListener(item.origin, item.value)); //Number listener
-OPERATORS.forEach(item => operatorListener(item)); //Operators listener
+function multiply (a, b) {
+    let result = Number(a) * Number(b);
+    return result
+};
+
+function division (a, b) {
+    let result = Number(a) / Number(b);
+    return result
+};
